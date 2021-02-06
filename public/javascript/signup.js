41df8c07cec69779b63b8dcc6a2a6defeb450844
ArlonTuazon <nikka1378@yahoo.com>
2021-02-06 00:35:22 -0800

    edit handlebars

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,31 +1,30 @@
 async function signupFormHandler(event) {
-    event.preventDefault();
-  
-    const username = document.querySelector('#userName').value.trim();
-    const email = document.querySelector('#emailAdd').value.trim();
-    const password = document.querySelector('#password1').value.trim();
-    
-    if (username && email && password) {
-      const response = await fetch('/api/users', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-        
-      });
-         console.log("#userName")
-      // check the response status
-      if (response.ok) {
+  event.preventDefault();
 
-        console.log('success');
-        document.location.replace('/dashboard');
-      } else {
-        alert(response.statusText);
-      }
-    }
-}
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
   
+
+  if (username && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    // check the response status
+    if (response.ok) {
+      console.log('success');
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
